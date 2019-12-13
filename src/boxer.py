e48168da2dbc7cd0834b703bce55d4455ebd3d22
Mickey Smith <smithmick14@gmail.com>
2019-12-13 13:50:11 -0600

    Wrote first few functions checking which images to scroll through for a dataset so the user can quit and reopen. Going to set up an actual git workflow now so I'm committing in case anything gets messed up

--- src/boxer.py
+++ src/boxer.py
@@ -4,8 +4,48 @@ import cv2
 from pathlib import Path
 
 def main():
+    print("\n\nWelcome to Bounding Boxer\n\n")
 
-    print("Welcome to Bounding Boxer")
+    data_path   = Path("data/sacral-labeling-template/avg-heatmaps")
+    output_path = Path("results/box-results/bounding_boxes.csv")
+
+    results_exist = touch_results_file(output_path)
+    labeled_paths = read_results_file(output_path) if results_exist else None
+    all_paths     = read_image_directory(data_path)
+    todo_paths    = compare_paths(all_paths, labeled_paths)
+
+    print(f"Total images: {len(all_paths)}")
+    print(f"Prelabeled images: {len(labeled_paths)}")
+    print(f"To be labelled: {len(todo_paths)}")
+
+
+def touch_results_file(output_path):
+    if not os.path.exists(output_path):
+        with open(output_path, 'w'): pass
+        return False
+    else:
+        return True
+
+def read_results_file(output_path):
+    paths = []
+    with open(output_path, 'r') as existing_results_file:
+        next(existing_results_file)
+        for line in existing_results_file.readlines():
+            paths.append(line.split(",")[0])
+
+    return paths
+
+def read_image_directory(data_path):
+    paths = []
+    for path in os.listdir(data_path):
+        if path[-4:] == ".png":
+            paths.append(path)
+
+    return paths
+
+def compare_paths(full_path, subset_path):
+    return list(set(full_path) - set(subset_path))
+
 
 if __name__ == "__main__":
     main()
